e85d22ac714d50b4a111cfaf30c6254faeb46901
Yeli.James <jbmalvar.cs.washington.edu>
2025-01-06 00:10:38 -0800

    Challenge #6

--- index.js
+++ index.js
@@ -97,21 +97,19 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
-
+    const gamesFilter = GAMES_JSON.filter((game) => game.pledged < game.goal);
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(gamesFilter);
 }
 
 // show only games that are fully funded
 function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
-    // use filter() to get a list of games that have met or exceeded their goal
-
-
-    // use the function we previously created to add unfunded games to the DOM
-
+    // use filter() to get a list of games that have not yet met their goal
+    const gamesFilter = GAMES_JSON.filter((game) => game.pledged > game.goal);
+    // use the function we previously created to add the unfunded games to the DOM
+    addGamesToPage(gamesFilter);
 }
 
 // show all games
@@ -119,7 +117,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON);
 }
 
 // select each button in the "Our Games" section
@@ -128,6 +126,8 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+
+
 
 
 /*************************************************************************************
