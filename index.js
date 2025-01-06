767ec3d2605c0eac2fa7ae1f638188df6f945063
Yeli.James <jbmalvar.cs.washington.edu>
2025-01-06 01:10:24 -0800

    Finish

--- index.js
+++ index.js
@@ -163,7 +163,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [first, second] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topPledge = document.createElement("p");
+topPledge.textContent = first.name;
+firstGameContainer.appendChild(topPledge);
 
 // do the same for the runner up item
+const runnerPledge = document.createElement("p");
+runnerPledge.textContent = second.name;
+secondGameContainer.appendChild(runnerPledge);
